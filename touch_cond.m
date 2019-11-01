7713960db88a24f330d7ef008a35c63ad631b005
Evan Jianchen Tung <dragonejt@gmail.com>
2019-11-01 16:11:31 -0700

    Added touch conditions file

---
+++ touch_cond.m
@@ -0,0 +1,18 @@
+rbc = new robot_class;
+isTouched = rbc.touched;
+if (isTouched == 1) 
+    %motors move 90 right
+    %move forward
+    if (isTouched == 1)
+        %motors move 180
+        %move forward
+        if (isTouched == 1)
+            %motors move 90 left
+            %move forward
+        end
+    else
+        %move forward
+    end
+else
+    %move forward
+end
