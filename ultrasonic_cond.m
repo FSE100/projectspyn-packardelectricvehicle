77819837976e10c877831c8b25066a1043434345
Kyle Sese <52438306+kyle-sese@users.noreply.github.com>
2019-11-01 16:52:34 -0700

    Update ultrasonic_cond.m

--- ultrasonic_cond.m
+++ ultrasonic_cond.m
@@ -1,15 +1,5 @@
-rbc = new robot_class;
+rbc = robot_class('EV3LL');
 distance = rbc.ultraVal;
 if (distance <= 30.48) 
-    %motors move 90 right
-    if (distance <= 30.48)
-        %motors move 180
-        if (distance <= 30.48)
-            %motors move 90 left
-        end
-    else
-        %move forward
-    end
-else
-    %move forward
+    %update map[i][j]
 end
