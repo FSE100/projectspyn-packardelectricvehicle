b39ac3eabeb6b228db70c85f14e7bb39e848d6a3
Evan Jianchen Tung <dragonejt@gmail.com>
2019-11-01 15:57:49 -0700

    Added Ultrasonic Conditions page

---
+++ ultrasonic_cond.m
@@ -0,0 +1,15 @@
+rbc = new robot_class;
+distance = rbc.ultraVal;
+if (distance <= 20) 
+    %motors move 90 right
+    if (distance <= 20)
+        %motors move 180
+        if (distance <= 20)
+            %motors move 90 left
+        end
+    else
+        %move forward
+    end
+else
+    %move forward
+end
